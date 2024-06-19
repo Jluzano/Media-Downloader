3adfa4d907c12d751df75c847bdaadcb472e58f2
Justin <justinluzano@gmail.com>
2024-06-18 23:54:58 -0700

    Resized window, added input field and download button

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
+#Importing modules
 from tkinter import *
 import tkinter as tk
 from pytube import YouTube
 
+# Initializing UI
 root = Tk()
+root.title("Media Downloader")
+root.geometry("400x200+500+300")
 
-myLabel = Label(root, text="Hello World!")
-myLabel.pack()
+title = Label(root, text="Insert Link Below:")
+title.grid(row=0, column=0, padx=10, pady=10)
+
+link = Entry(root, width=50)
+link.grid(row=1, column=0)
+
+button = Button(root, text='Download', width=10)
+button.grid(row=1, column=1)
 
 root.mainloop()
